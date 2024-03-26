52a1f548535136edde62db6e1536fb6efad26042
Liubov5 <liubov.iakovleva5@gmail.com>
2024-03-26 23:10:11 +0300

    fix

--- script.js
+++ script.js
@@ -15,17 +15,22 @@ let participants_left_arrow = document.querySelector(".participants__switcher-le
 let participants_right_arrow = document.querySelector(".participants__switcher-right");
 let participants_container = document.querySelector(".participants__container");
 let participants_switcher_count = document.querySelector(".participants__switcher__count")
-let participants_number = document.querySelector(".participants__number");
+let participants_number_elem = document.querySelector(".participants__number");
 
 let container = document.querySelector(".container");
-let participants_step_value = 0;
-let participants_step_count = 0;
-let participants_count
 
 let dots_element = [];
 let value_stage = 0;
 let dots_number;
 let dots_index = 0;
+
+
+let participants_number_step_value = 0; //шаг индикатора 1 или 3
+let participants_number_indicator; //кол-во участников для индикатора
+let participants_step_value = 0; //шаг изменения карусели
+let participants_value = 0; //общее значение изменения значения карусели
+let participants_side_by_time = 'right'; // в какую сторону меняется карусель по времени
+
 window.onload = () => {
     dots_number = stages_container.clientWidth / window.innerWidth;
     dots_number = Math.ceil(dots_number);
@@ -39,17 +44,18 @@ window.onload = () => {
 
     if(window.innerWidth < 767) {
         participants_step_value = window.innerWidth
-        participants_step_count = 1;
+        participants_number_step_value = 1;
     } else {
         participants_step_value = container.clientWidth;
-        participants_step_count = 3;
+        participants_number_step_value = 3;
     }
-    participants_number.innerHTML = participants_step_count;
-    participants_count = participants_step_count;
+    participants_number_elem.innerHTML =  participants_number_step_value;
+    participants_number_indicator =  participants_number_step_value;
+
+    setInterval(participants_switcher_by_time, 4000);
 
 }
 
-//добавить свайп
 stages_left_arrow.addEventListener('click', ()=> {
     if (value_stage < 0) {
         dots_element[dots_index].classList.remove("active")
@@ -65,6 +71,7 @@ stages_left_arrow.addEventListener('click', ()=> {
         } 
     } 
 })
+
 
 stages_right_arrow.addEventListener('click', ()=> {
      if(Math.abs(value_stage) < Math.abs((window.innerWidth * (dots_number - 1) ))) {
@@ -82,44 +89,87 @@ stages_right_arrow.addEventListener('click', ()=> {
     } 
 })
 
-let participants_value = 0;
+let participants_to_right_switcher = () => {
+    if(Math.abs(participants_value) < Math.abs((participants_container.clientWidth ) - Math.abs(participants_step_value) )) {
+      
+        participants_left_arrow.classList.add("active");
+        participants_right_arrow.classList.remove("disabled");
 
-participants_left_arrow.addEventListener('click', ()=>{
+        participants_number_indicator += participants_number_step_value;
+        participants_number_elem.innerHTML = participants_number_indicator;
+
+        participants_value -= participants_step_value;
+        participants_container.style.transform = `TranslateX(${participants_value}px)`;
+        
+        if (Math.abs(participants_value) == Math.abs(participants_container.clientWidth) - Math.abs(participants_step_value) ) {
+            participants_right_arrow.classList.add("disabled");
+            participants_right_arrow.classList.remove("active");
+            participants_side_by_time='left'
+        }  
+    }
+}
+
+let participants_to_left_switcher = () => {
     if(participants_value < 0 ) {
         participants_right_arrow.classList.add("active");
         participants_left_arrow.classList.remove("disabled");
 
-        participants_count -=participants_step_count;
-        participants_number.innerHTML = participants_count;
+        participants_number_indicator -= participants_number_step_value;
+        participants_number_elem.innerHTML = participants_number_indicator;
 
         participants_value += participants_step_value;
         participants_container.style.transform = `TranslateX(${participants_value}px)`;
         if (participants_value == 0) {
             participants_left_arrow.classList.remove("active");
             participants_left_arrow.classList.add("disabled");
+            participants_side_by_time = 'right'
         } 
     }
-})
+}
 
-participants_right_arrow.addEventListener('click', ()=>{
-    
-    if(Math.abs(participants_value) < Math.abs((participants_container.clientWidth ) - Math.abs(participants_step_value) )) {
+participants_left_arrow.addEventListener('click', participants_to_left_switcher);
+
+participants_right_arrow.addEventListener('click', participants_to_right_switcher);
+
+let participants_switcher_by_time = () => {
+    if(participants_side_by_time === 'right') {
+        participants_to_right_switcher();
+       
+    } else  if ( participants_side_by_time === 'left') {
+        participants_to_left_switcher();
+    }
+}
+
+// let participants_switcher = () => {
+//     if(participants_value < 0 ) {
+//         participants_right_arrow.classList.add("active");
+//         participants_left_arrow.classList.remove("disabled");
+
+//         participants_count -=participants_step_count;
+//         participants_number.innerHTML = participants_count;
+
+//         participants_value += participants_step_value;
+//         participants_container.style.transform = `TranslateX(${participants_value}px)`;
+//         if (participants_value == 0) {
+//             participants_left_arrow.classList.remove("active");
+//             participants_left_arrow.classList.add("disabled");
+//         } 
+//     }
+
+//     else if(Math.abs(participants_value) < Math.abs((participants_container.clientWidth ) - Math.abs(participants_step_value) )) {
       
-        participants_left_arrow.classList.add("active");
-        participants_right_arrow.classList.remove("disabled");
+//         participants_left_arrow.classList.add("active");
+//         participants_right_arrow.classList.remove("disabled");
 
-        participants_count +=participants_step_count;
-        participants_number.innerHTML = participants_count;
+//         participants_count +=participants_step_count;
+//         participants_number.innerHTML = participants_count;
 
-        participants_value -= participants_step_value;
-        participants_container.style.transform = `TranslateX(${participants_value}px)`;
+//         participants_value -= participants_step_value;
+//         participants_container.style.transform = `TranslateX(${participants_value}px)`;
         
-        if (Math.abs(participants_value) == Math.abs(participants_container.clientWidth) - Math.abs(participants_step_value) ) {
-           
-            participants_right_arrow.classList.add("disabled");
-            participants_right_arrow.classList.remove("active");
-        }
-        
-    } 
-    
-})
+//         if (Math.abs(participants_value) == Math.abs(participants_container.clientWidth) - Math.abs(participants_step_value) ) {  
+//             participants_right_arrow.classList.add("disabled");
+//             participants_right_arrow.classList.remove("active");
+//         }  
+//     } 
+// }
